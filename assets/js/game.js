7753f3e85ccfefdafe0f5208651ccdb36d48b691
BBUTH <BBUTH_EMAIL>
2018-08-17 08:48:06 -0700

    finished rough version of finished game

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,7 +51,7 @@ let Balrog = {
 }
 
 
-/** example health bar
+/** 
 let currentHealth = 100;
 $(".health-bar").click(function(){
     let nextHealth = (currentHealth - 10)
@@ -68,15 +68,18 @@ let MS_RPG = {
     charSelected: "none",
     charStats: "none",
     charCurrentHealth: 0,
+    charCurrentAtk: 0,
 
     maxEnemyCount: enemyCount,
     currEnemyCount: 0,
 
     defSelected: "none",
     defStats: "none",
-    defCurrentHeath: 0,
+    defCurrentHealth: 0,
 
     phase: 0,
+    aFlag: 0,
+    newMon: 0,
     // phase 0 = choose character
     // phase 1 = choose defender
     // phase 2 = battle defender
@@ -156,20 +159,21 @@ let MS_RPG = {
             }
         }
         /** CAN CLICK ON CHAR HERE */
-        $(document).ready(function() {
-            $('.imgCharBox').on("click", function() {
-                if (MS_RPG.phase === 0) {
-                    //choose character
-                    console.log($(this).attr("data"))
-                    MS_RPG.selectedCharacter($(this).attr("data"))
-                }
+        if (MS_RPG.phase === 0) {
+            $(document).ready(function() {
+                $('.imgCharBox').on("click", function() {
+                        //choose character
+                    if (MS_RPG.phase === 0) {
+                        MS_RPG.selectedCharacter($(this).attr("data"))
+                    }
+                })
             })
-        })
+        }
         /** END */
     },
     // updates RPG with character selected
     selectedCharacter: function (str) {
-        this.phase++ //phase 1
+        this.phase++ //phase 1, cannnot choose another character
         this.updateAnnouncement('You Haven Chosen ' + str)
         switch (str) {
             case 'Warrior':
@@ -189,94 +193,173 @@ let MS_RPG = {
                 this.charStats = Thief
                 break
         }
-        this.selectDefenderScreen()       
+        this.charCurrentHealth = this.charStats.health
+        this.charCurrentAtk = this.charStats.attack
+
+        this.gameScreenInit()       
     },
-    // counts & displays monsters 
-    selectDefenderScreen: function () {
+    gameScreenInit: function () {
         $('.gameScreen').empty()
         this.currEnemyCount = this.maxEnemyCount
+        /**
+         * .gameScreen
+         * >> .gameNames
+         */
+        $('.gameScreen').append(`<div class="col s12 gameNames">
+        </div>`)
 
-        /* [you]_[defender]_[d1][d2][d3] */
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         */
         for (let i =0; i < 5; i++) {
             if(i === 0){
-            $('.gameScreen').append(`<div class="col s2 center">
+            $('.gameNames').append(`<div class="col s2 center">
             <h5>YOU</h5>
             </div>`)
             }else if(i === 1 || i === 3){
-                $('.gameScreen').append(`<div class="col s1">
+                $('.gameNames').append(`<div class="col s1">
                 </div>`)
             }else if(i === 2){
-                $('.gameScreen').append(`<div class="col s2 center">
+                $('.gameNames').append(`<div class="col s2 center">
                 <h5>Defender</h5>
                 </div>`)
             }else{
-                $('.gameScreen').append(`<div class="col s6 center">
+                $('.gameNames').append(`<div class="col s6 center">
                 <h5>Next Oppenents Area</h5>
                 </div>`) 
             }
         }
 
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> .gameModels
+         */
+        $('.gameScreen').append(`<div class="col s12 gameModels">
+        </div>`)
+
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[EMPTY<emptyatm>]][SPACER][[MON1][MON2][MON3]]
+         */
         for (let i =0; i < 4; i++) {
             if(i === 0){
-                $('.gameScreen').append(`<div class="col s2">
+                $('.gameModels').append(`<div class="col s2">
                 <div class="imgCharBox ${'imgChar'+this.charSelected} flip" data="${this.charSelected}"></div>
                 </div>`)
             }else if(i === 1 || i === 3){
-                $('.gameScreen').append(`<div class="col s1 spacer">
+                $('.gameModels').append(`<div class="col s1 spacer">
                 </div>`)
             }else if(i === 2){
-                $('.gameScreen').append(`<div class="col s2 defender">
+                $('.gameModels').append(`<div class="col s2 defenderModel">
                 </div>`)
             }
         }
         for (let i =0; i < enemyCount; i++) {
-            $('.gameScreen').append(`<div class="col s2 nextOpponent ${enemyOptions[i]}">
+            $('.gameModels').append(`<div class="col s2 nextOpponent ${enemyOptions[i]}">
             <div class="imgMonBox imgCharBorder ${'imgMon'+enemyOptions[i]}" data="${enemyOptions[i]}"></div>
             </div>`) 
         }
 
         // creates character health bar and character stats
-        $('.gameScreen').append(`
-        <div class="col s12 rowHealthbar">
-            <div class="col s2 center">
-                <div class="health-bar">
-                    <div class="health-bar-glass">
-                        <div class="health-bar-fluid" data="${this.charSelected}"></div>
-                    </div>
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[EMPTY<emptyatm>]][SPACER][[MON1][MON2][MON3]]
+         * >> .rowHealthBar
+         * >> >> [YOU]
+         */
+        $('.gameScreen').append(`<div class="col s12 rowHealthbar">
+        </div>
+        `)
+        $('.rowHealthbar').append(`<div class="col s2 center">
+            <div class="health-bar">
+                <div class="health-bar-glass">
+                    <div class="health-bar-fluid ${this.charSelected}"></div>
                 </div>
             </div>
-        </div>
+        </div>`)
+
+
+        // creates character health bar and character stats
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[DEFENDER<emptyatm>]][SPACER][[MON1][MON2][MON3]]
+         * >> .rowHealthBar
+         * >> >> [YOU]
+         * >> .RowStats
+         * >> >> .charStats
+         * >> >> .spacer
+         * >> >> .defStats
+         */
+        $('.gameScreen').append(`
         <div class="col s12 rowStats">
             <div class="col s2 charStats"></div>
             <div class="col s1 spacer"></div>
             <div class="col s2 defStats"></div>
         </div>
         `)
-        this.updateCharStats()
 
-        this.updateAnnouncement('Select a Monster to Attack')
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[DEFENDER<emptyatm>]][SPACER][[MON1][MON2][MON3]]
+         * >> .rowHealthBar
+         * >> >> [YOU]
+         * >> .RowStats
+         * >> >> .charStats
+         * >> >> >> [HP][ATK][CATK][RATK]
+         * >> >> .spacer
+         * >> >> .defStats
+         */
+        this.updateCharStats()
         this.selectDefenderLoop()
     },
     // on click for nextOpponent 
     selectDefenderLoop: function () {
-        /** CAN CLICK ON MONSTERS TO BE DEFENDER */
-        $(document).ready(function() {     
-            $('.imgMonBox').on("click", function() {
-                if (MS_RPG.phase === 1) {
+        this.updateAnnouncement('Select Next Monster to Attack')
+        if (this.aFlag === 0) {
+            $(document).ready(function() {     
+                $('.nextOpponent .imgMonBox').on("click", function() {
                     //choose Defender
-                    console.log($(this).attr("data"))
-                    MS_RPG.selectedDefenderScreen($(this).attr("data"))
-                }
-            })  
-        })
+                    if(MS_RPG.phase === 1){
+                        MS_RPG.selectedDefenderScreen($(this).attr("data"))
+                    }
+                })    
+            })
+        }
     },
     // updates defender stats 
     selectedDefenderScreen: function (str) {
-        //updates phase to 2
-        this.phase++
-
+        this.phase++ //phase is 2, cannot choose another defender until it goes back
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[DEFENDER<MON1>]][SPACER][[MON2][MON3]]
+         * >> .rowHealthBar
+         * >> >> [YOU]
+         * >> .RowStats
+         * >> >> .charStats
+         * >> >> >> [HP][ATK][CATK][RATK]
+         * >> >> .spacer
+         * >> >> .defStats
+         */
         //removes selected monster and puts it on defender area
-        $(`.imgMon${str}`).clone().appendTo('.defender')
+        $(`.imgMon${str}`).clone().appendTo('.defenderModel')
         $(`.nextOpponent.${str}`).remove()
 
         //updates current defender
@@ -294,56 +377,155 @@ let MS_RPG = {
                 this.defStats = Balrog
                 break
         }
-
-        // adds hp bar for defender if it hasnt been added aka curr = max enemies
-        if(this.currEnemyCount === this.maxEnemyCount){
+        this.defCurrentHealth = this.defStats.health
+        if(this.aFlag === 0){
+            // adds hp bar for defender if it hasnt been added aka curr = max enemies
+            /**
+             * .gameScreen
+             * >> .gameNames
+             * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+             * >> .gameModels
+             * >> >> [YOU][SPACER][[DEFENDER<MON1>]][SPACER][[MON2][MON3]]
+             * >> .rowHealthBar
+             * >> >> [YOU][SPACER][DEFENDER]
+             * >> .RowStats
+             * >> >> .charStats
+             * >> >> >> [HP][ATK][CATK][RATK]
+             * >> >> .spacer
+             * >> >> .defStats
+             */
             $('.rowHealthbar').append(`
                 <div class="col s1 spacer"></div>
                 <div class="col s2 center">
                     <div class="health-bar">
                         <div class="health-bar-glass">
-                            <div class="health-bar-fluid" data="${this.charSelected}"></div>
+                            <div class="health-bar-fluid defenderHealth"></div>
                         </div>
                     </div>
                 </div>
             `)
         }
-
+        $('.health-bar-fluid.defenderHealth').addClass(`${this.defSelected}`)
         // adds the defender stats
+        /**
+         * .gameScreen
+         * >> .gameNames
+         * >> >> [YOU][SPACER][DEFENDER][SPACER][NEXT OPPONENTS AREA]
+         * >> .gameModels
+         * >> >> [YOU][SPACER][[DEFENDER<MON1>]][SPACER][[MON2][MON3]]
+         * >> .rowHealthBar
+         * >> >> [YOU][SPACER][DEFENDER]
+         * >> .RowStats
+         * >> >> .charStats
+         * >> >> >> [HP][ATK][CATK][RATK]
+         * >> >> .spacer
+         * >> >> .defStats
+         * >> >> >> [HP][CATK]
+         */
         this.updateDefStats()
+        this.updateDefHp()
 
         //removes border around next opponent area
-        $(`.nextOpponent .imgCharBorder`).removeClass('imgCharBorder')
+        $(`.nextOpponent .imgMonBox`).removeClass('imgCharBorder')
 
         //trigger fighting between char and defender
+        this.updateAnnouncement(`Click on the ${str} to Attack`)
+        this.phase++
         this.fightDefenderScreen(str)
     },
     // fights and goes back into selectDefenderLoop if monster available or not dead yet
     fightDefenderScreen: function (str){
-        this.updateAnnouncement(`Click on the ${str} to Attack`)
+        //if the character or defender is not dead
+        if(this.charCurrentHealth > 0  && this.defCurrentHealth > 0) {
+            // gotta redeclare click due to DOM remove and ad
+            if (this.newMon === 0) {
+                this.newMon = 1
+                this.aFlag = 1
+                $(document).ready(function() {     
+                    $('.defenderModel .imgMonBox').on("click", function() {
+                        //choose Defender
+                        $(`.imgChar${MS_RPG.charSelected}`).addClass(`attack${MS_RPG.charSelected}`)
+                        // MS_RPG.fightDefenderScreen(str)
+                        setTimeout(function() {
+                            $(`.imgChar${MS_RPG.charSelected}`).removeClass(`attack${MS_RPG.charSelected}`)
+                        }, 500)
+
+                        MS_RPG.defCurrentHealth = MS_RPG.defCurrentHealth - MS_RPG.charCurrentAtk
+                        MS_RPG.charCurrentHealth = MS_RPG.charCurrentHealth - MS_RPG.defStats.counter
+                        MS_RPG.charCurrentAtk = MS_RPG.charCurrentAtk + MS_RPG.charStats.attackRaised
+
+                        MS_RPG.updateCharStats()
+                        MS_RPG.updateDefStats()
+
+                        MS_RPG.updateCharHp()
+                        MS_RPG.updateDefHp()
+
+                        MS_RPG.fightDefenderScreen()
+                    })  
+                })
+            }
+        }else if(MS_RPG.charCurrentHealth > 0 && enemyCount > 0){ // you can continue fighting, also enemy hp <= 0
+            this.currEnemyCount--
+            $('.defenderModel').empty()
+            $('.health-bar-fluid.defenderHealth').removeClass(`${this.defSelected}`)
+            // last monster is just defeated
+            if(this.currEnemyCount === 0){
+                MS_RPG.gameEnd()
+            }else{ // more enemies to defeat
+                $(`.nextOpponent .imgMonBox`).addClass('imgCharBorder')
+                this.phase = 1
+                this.newMon = 0
+                this.selectDefenderLoop()
+            }
+        }
     },
     updateAnnouncement: function (str) {
         $('.announcementBanner').html(`<h4>${str}</h4>`)
     },
     updateCharStats: function () {
+        let redText = ''
+        let greenText = ''
+        if (this.charCurrentHealth !== this.charStats.health) {
+            redText = `class="red-text"`
+        }
+        if (this.charCurrentAtk !== this.charStats.attack) {
+            greenText = `class="green-text"`
+        }
         $('.charStats').html(`
-            <h5>${'HP: ' + this.charStats.health}</h5>
-            <h5>${'ATK: ' + this.charStats.attack}</h5>
+            <h5><b ${redText}>${'HP: ' + this.charCurrentHealth}</b></h5>
+            <h5><b ${greenText}>${'ATK: ' + this.charCurrentAtk}</b></h5>
             <h5>${'CATK: ' + this.charStats.counter}</h5>
             <h5>${'RATK: ' + this.charStats.attackRaised}</h5>
         `)
     },
     updateDefStats: function () {
+        let redText = ''
+        if (this.defCurrentHealth !== this.defStats.health) {
+            redText = `class="red-text"`
+        }
         $('.defStats').html(`
-            <h5>${'HP: ' + this.defStats.health}</h5>
+            <h5><b ${redText}>${'HP: ' + this.defCurrentHealth}</b></h5>
             <h5>${'CATK: ' + this.defStats.counter}</h5>
         `)
     },
+    updateCharHp: function () {
+        let percent = (this.charCurrentHealth / this.charStats.health) * 100
+        $(`.health-bar-fluid.${this.charSelected}`).css( "width", `${percent}%` );
+    },
+    updateDefHp: function () {
+        let percent = (this.defCurrentHealth / this.defStats.health) * 100
+        $(`.health-bar-fluid.${this.defSelected}`).css( "width", `${percent}%` );
+    },
     runRPG: function () {
         this.selectCharacterScreen()
+    },
+    gameEnd: function () {
+        this.updateAnnouncement('YOU WIN!')
     }
 }
 
 $(document).ready(function() {
+
     MS_RPG.runRPG()
+    
 })
